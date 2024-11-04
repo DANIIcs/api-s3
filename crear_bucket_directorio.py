990e07e0bd0584ce539b823f1032fd8867b3e8fc
Daniel Caballero <154714304+DANIIcs@users.noreply.github.com>
2024-11-03 22:18:24 -0500

    Add files via upload

--- crear_bucket_directorio.py
+++ crear_bucket_directorio.py
@@ -1,16 +1,27 @@
 import boto3
+import json
 
 def lambda_handler(event, context):
-    bucket_name = event["body"]["bucket_name"]
-    directorio = event["body"]["directorio"]
-
     try:
+        # Decodificar el cuerpo si es necesario
+        body = json.loads(event["body"])
+
+        bucket_name = body["bucket_name"]
+        directorio = body["directorio"]
+
         s3 = boto3.client("s3")
         # Crear un objeto vacío para representar el directorio
         s3.put_object(Bucket=bucket_name, Key=(directorio + '/'))
+        
         return {
             "statusCode": 201,
             "message": f"Directorio '{directorio}' creado exitosamente en el bucket '{bucket_name}'."
+        }
+
+    except KeyError as e:
+        return {
+            "statusCode": 400,
+            "message": f"Falta un parámetro en la solicitud: {str(e)}"
         }
 
     except Exception as e:
